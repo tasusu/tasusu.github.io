8ba45b73d2d4d8a08a47959e53adab3ae128a2fc
tasusu <tasuku.soma+github@gmail.com>
2014-03-28 15:55:24 +0900

    small fixes

--- conf.py
+++ conf.py
@@ -74,6 +74,7 @@ exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
+highlight_language = 'latex'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
